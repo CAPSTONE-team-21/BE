4a48bcf732874515d5405713db7446fea43efd26
haeyoon1 <hynam1220@naver.com>
2025-04-05 20:37:36 +0900

    feat: 채팅 관련 service 작성

--- src/main/java/org/sspoid/sspoid/db/chatmassage/ChatMessageRepository.java
+++ src/main/java/org/sspoid/sspoid/db/chatmassage/ChatMessageRepository.java
@@ -3,6 +3,9 @@ package org.sspoid.sspoid.db.chatmassage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+    List<ChatMessage> findByChatSession_Id(Long id);
 }
