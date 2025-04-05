8b57e70026209929918857bcec57c0208acdec04
haeyoon1 <hynam1220@naver.com>
2025-04-05 16:26:54 +0900

    feat: Initial Spring Boot project from Spring Initializr

---
+++ src/main/java/org/sspoid/sspoid/SspoidApplication.java
@@ -0,0 +1,13 @@
+package org.sspoid.sspoid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SspoidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SspoidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sspoid/sspoid/SspoidApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sspoid.sspoid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SspoidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
