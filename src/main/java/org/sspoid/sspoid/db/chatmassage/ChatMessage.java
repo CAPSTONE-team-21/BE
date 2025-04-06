7fea7dfa2be4dcbb11fb83130ba3d3792a18b707
haeyoon1 <hynam1220@naver.com>
2025-04-07 02:42:22 +0900

    refactor: 엔티티 수정

--- src/main/java/org/sspoid/sspoid/db/chatmassage/ChatMessage.java
+++ src/main/java/org/sspoid/sspoid/db/chatmassage/ChatMessage.java
@@ -1,6 +1,8 @@
 package org.sspoid.sspoid.db.chatmassage;
 
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -10,15 +12,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.sspoid.sspoid.db.BaseEntity;
-import org.sspoid.sspoid.db.chatsession.ChatSession;
 import org.sspoid.sspoid.db.chatsession.SkinType;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -32,18 +34,20 @@ public class ChatMessage extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "chat_session_id", nullable = false)
-    private ChatSession chatSession;
+    @Column(name = "chat_session_id", nullable = false)
+    private Long chatSessionId;
 
     @Column
     @Enumerated(EnumType.STRING)
     private SenderType sender;
 
-
-    @Column(name = "skin_type")
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(
+            name = "skin_types",
+            joinColumns = @JoinColumn(name = "chat_message_id")
+    )
     @Enumerated(EnumType.STRING)
-    private SkinType skinType;
+    private List<SkinType> skinTypes;
 
     @Lob
     @Column(name = "message", nullable = false)
