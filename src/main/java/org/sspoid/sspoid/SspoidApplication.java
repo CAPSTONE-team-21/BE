e0b94e51617c50c63709e9a43c0579f18f58d541
haeyoon1 <hynam1220@naver.com>
2025-04-30 17:19:58 +0900

    feat: application 파일 수정

--- src/main/java/org/sspoid/sspoid/SspoidApplication.java
+++ src/main/java/org/sspoid/sspoid/SspoidApplication.java
@@ -2,7 +2,9 @@ package org.sspoid.sspoid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SspoidApplication {
 
