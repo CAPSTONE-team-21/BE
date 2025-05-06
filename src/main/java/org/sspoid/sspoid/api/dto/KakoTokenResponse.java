00d258cb1228de918e9e02c5cdf8424d277211bf
haeyoon1 <hynam1220@naver.com>
2025-05-07 02:54:32 +0900

    feat: kakao 로그인 dto 생성

---
+++ src/main/java/org/sspoid/sspoid/api/dto/KakoTokenResponse.java
@@ -0,0 +1,24 @@
+package org.sspoid.sspoid.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class KakoTokenResponse {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("expires_in")
+    private Integer expiresIn;
+
+    @JsonProperty("scope")
+    private String scope;
+}
