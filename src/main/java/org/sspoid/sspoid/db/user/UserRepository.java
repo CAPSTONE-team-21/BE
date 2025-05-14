f30f34bcdc0a8a80c098fd78dd4f69771e044ee8
haeyoon1 <hynam1220@naver.com>
2025-05-14 16:05:25 +0900

    feat: user repository 메서드 추가

--- src/main/java/org/sspoid/sspoid/db/user/UserRepository.java
+++ src/main/java/org/sspoid/sspoid/db/user/UserRepository.java
@@ -1,6 +1,13 @@
 package org.sspoid.sspoid.db.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
