3970c4f125452e2b84d6722603023b28472b66a0
haeyoon1 <hynam1220@naver.com>
2025-05-14 16:08:29 +0900

    feat: 코드 인증 클래스 생성

---
+++ src/main/java/org/sspoid/sspoid/api/dto/auth/VerificationCode.java
@@ -0,0 +1,20 @@
+package org.sspoid.sspoid.api.dto.auth;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class VerificationCode {
+
+    private String code;
+
+    private LocalDateTime expiresTime;
+}
