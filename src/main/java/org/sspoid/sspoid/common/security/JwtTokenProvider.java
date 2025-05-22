89c6c48c88de63328984463e6fab48b0ebc8264f
haeyoon1 <hynam1220@naver.com>
2025-05-23 02:41:44 +0900

    feat: token에서 user id 가져오는 메서드 생성

--- src/main/java/org/sspoid/sspoid/common/security/JwtTokenProvider.java
+++ src/main/java/org/sspoid/sspoid/common/security/JwtTokenProvider.java
@@ -3,9 +3,11 @@ package org.sspoid.sspoid.common.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.sspoid.sspoid.db.user.User;
+import org.sspoid.sspoid.db.user.UserRepository;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
@@ -22,12 +24,15 @@ public class JwtTokenProvider {
 
     private final SecretKey secretKey;
 
+    private final UserRepository userRepository;
+
     private static final String JWT_CLAIMS = "email";
 
-    public JwtTokenProvider(@Value("${jwt.secret}") String secret) {
+    public JwtTokenProvider(@Value("${jwt.secret}") String secret, UserRepository userRepository) {
         this.secretKey = new SecretKeySpec(
                 secret.getBytes(StandardCharsets.UTF_8),
                 "HmacSHA256");
+        this.userRepository = userRepository;
     }
 
     public String createAccessToken(User user) {
@@ -63,4 +68,12 @@ public class JwtTokenProvider {
                 .parseSignedClaims(refreshToken)
                 .getPayload();
     }
+
+    public Long getUserIdFromToken(String token) {
+        String email = getClaimsToken(token).get("email", String.class);
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+        return user.getId();
+    }
+
 }
