2a497bac85a798e9c76d04ced9f4419ebd258bc8
haeyoon1 <hynam1220@naver.com>
2025-05-22 15:50:23 +0900

    feat: 요약 모델 request dto 변경 (skintype 리스트로)

--- src/main/java/org/sspoid/sspoid/api/dto/model/SummaryModelRequest.java
+++ src/main/java/org/sspoid/sspoid/api/dto/model/SummaryModelRequest.java
@@ -3,9 +3,11 @@ package org.sspoid.sspoid.api.dto.model;
 import org.sspoid.sspoid.db.chatmassage.SenderType;
 import org.sspoid.sspoid.db.chatsession.SkinType;
 
+import java.util.List;
+
 public record SummaryModelRequest (
         SenderType sender,
-        SkinType skinType,
+        List<SkinType> skinType,
         String message
 ){
 }
